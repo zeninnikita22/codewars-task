6a42987a3f6a96a1ef5987494052c07d7de2dc8e
zeninnikita22 <zeninnikita@gmail.com>
2022-02-09 00:06:34 +0200

    updated the solution

--- codewars-task1.js
+++ codewars-task1.js
@@ -1,6 +1,24 @@
+// Our football team finished the championship. The result of each match look like "x:y".
+// Results of all matches are recorded in the collection.
+// For example: ["3:1", "2:2", "0:1", ...]
+// Write a function that takes such collection and counts the points of our team in the championship.
+// Rules for counting points for each match:
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+// Notes:
+// there are 10 matches in the championship
+
 function points(games) {
   let numberOfPoints = 0;
-  for (i = 0; i <= games.length - 1; i++) {}
+  for (i = 0; i <= games.length - 1; i++) {
+    if (games[i].charAt(0) > games[i].charAt(2)) {
+      numberOfPoints = numberOfPoints + 3;
+    } else if (games[i].charAt(0) === games[i].charAt(2)) {
+      numberOfPoints = numberOfPoints + 1;
+    } else {
+      numberOfPoints = numberOfPoints + 0; //I know that basically this is not needed but still inluded just to see all if conditions
+    }
+  }
+  return numberOfPoints;
 }
-
-["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"];
