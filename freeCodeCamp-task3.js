241cbe6d8bb7b86cf3b91663a5a349db12b92d51
zeninnikita22 <zeninnikita@gmail.com>
2022-04-02 23:00:43 +0300

    added new freeCodeCamp tasks solutions

--- freeCodeCamp-task3.js
+++ freeCodeCamp-task3.js
@@ -9,3 +9,34 @@ function repeatStringNumTimes(str, num) {
   }
   return str;
 }
+
+// Truncate a string (first argument) if it is longer than the given maximum string length
+// (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  const newStr = str.slice(0, num);
+  if (str.length <= num) {
+    return newStr;
+  }
+  return newStr + "...";
+}
+
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  const newSecondArray = arr2.slice();
+  newSecondArray.splice(n, 0, ...arr1.slice());
+  return newSecondArray;
+}
+
+// Return the lowest index at which a value (second argument) should be inserted into an array
+// (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort((a, b) => a - b);
+  return arr.findIndex((element) => element === num);
+}
