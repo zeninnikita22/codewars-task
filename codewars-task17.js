a29445d688f1ee4cb2ebfd48a76854e0bfe88ea7
zeninnikita22 <zeninnikita@gmail.com>
2022-04-05 21:25:38 +0300

    added new kata solution

--- codewars-task17.js
+++ codewars-task17.js
@@ -1,23 +1,3 @@
-// Given a string made of digits [0-9],
-// return a string where each digit is repeated a number of times equals to its value.
-
-// Examples
-// explode("312")
-// should return :
-
-// "333122"
-// explode("102269")
-// should return :
-
-// "12222666666999999999"
-
-function explode(str) {
-  return str
-    .split("")
-    .map((element) => element.repeat(element))
-    .join("");
-}
-
 // Write a function that takes a single string (word) as argument. The function must return an ordered
 // list containing the indexes of all capital letters in the string.
 
