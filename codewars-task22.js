550b41fef3feb4fdbffa9bdfc3273f0b9c2eea05
zeninnikita22 <zeninnikita@gmail.com>
2022-04-14 13:22:34 +0300

    added new kata solution

---
+++ codewars-task22.js
@@ -0,0 +1,35 @@
+// We want to know the index of the vowels in a given word, for example,
+// there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+
+const str = "YoMama";
+
+function findIndex(str) {
+  const arr = str.toLowerCase().split("");
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] === "a" ||
+      arr[i] === "e" ||
+      arr[i] === "i" ||
+      arr[i] === "o" ||
+      arr[i] === "u" ||
+      arr[i] === "y"
+    ) {
+      newArr.push(i + 1);
+    }
+  }
+  return newArr;
+}
+
+console.log(findIndex(str));
